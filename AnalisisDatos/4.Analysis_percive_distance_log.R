db21808bdb61edfbd697848d38bd243f772ed5f3
elombera <57369042+elombera@users.noreply.github.com>
2023-10-13 11:52:43 -0300

    sdasd
    asdasad

--- AnalisisDatos/4.Analysis_percive_distance_log.R
+++ AnalisisDatos/4.Analysis_percive_distance_log.R
@@ -12,6 +12,7 @@ results_tbl$interceptoLog = 0
 fig_normal = list()
 for (i in 1:length(levels(results_tbl$subject))) {
   print(i)
+  i = 2
   sub = levels(results_tbl$subject)[i]
   print(sub)
   m.pend = lm(log10(perc_dist) ~ log10(target_distance)*condition,
@@ -24,13 +25,24 @@ for (i in 1:length(levels(results_tbl$subject))) {
   
   cbPalette <- c("#000000","#E69F00","#009E73", "#999999", "#D55E00", "#0072B2", "#CC79A7", "#F0E442")
   
-  
+  x1 = c(0:8)
+  # x1 = seq(1,8,by=.01)
+  curves1 = data.frame(x2 = x1,
+             y1 = (10^(m.pend$coefficients[[1]]))*(x1^m.pend$coefficients[[2]]),
+             condition = "Ear level")
+  curves2 = data.frame(x2 = x1,
+                      y1 = 10^(m.pend$coefficients[[1]]+m.pend$coefficients[[3]])*(x1^(m.pend$coefficients[[2]]+m.pend$coefficients[[4]])),
+                      condition = "Floor level")
+  curve = merge(x = curves1, y = curves2, all = TRUE)
+  # bearlevel = function(x) (10^(m.pend$coefficients[[1]]))*(x^m.pend$coefficients[[2]])
+  # bfloorlevel = function(x) (10^(m.pend$coefficients[[1]]+m.pend$coefficients[[3]]))*(x^(m.pend$coefficients[[2]]+m.pend$coefficients[[4]]))
+  # 
   eq1 <- substitute("Ear level:"~~~italic(y) == a %.% italic(X)^italic((b)), 
-                    list(a = round(m.pend$coefficients[[2]],digits = 2),
-                         b = round(10^(m.pend$coefficients[[1]]), digits = 2)))
+                    list(a = round(10^(m.pend$coefficients[[1]]),digits = 2),
+                         b = round(m.pend$coefficients[[2]], digits = 2)))
   eq2 <- substitute("Floor level:"~~~italic(y) == a %.% italic(X)^italic((b)), 
-                    list(a = round(m.pend$coefficients[[2]]+m.pend$coefficients[[4]], digits = 2),
-                         b = round(10^(m.pend$coefficients[[1]]+m.pend$coefficients[[3]]), digits = 2)))
+                    list(a = round(10^(m.pend$coefficients[[1]]+m.pend$coefficients[[3]]), digits = 2),
+                         b = round(m.pend$coefficients[[2]]+m.pend$coefficients[[4]], digits = 2)))
   eq3 <- substitute("r.squared:"~~~italic(R)^italic(2) == italic(b), 
                     list(b = round(summary(m.pend)$r.squared, digits = 2)))
   
@@ -47,17 +59,10 @@ for (i in 1:length(levels(results_tbl$subject))) {
                 intercept = 0, 
                 alpha = 0.5, 
                 linetype = "dashed") +
-    geom_function(fun = function(x) (10^(m.pend$coefficients[[1]]))*(x^m.pend$coefficients[[2]]))+
-    # curve(m.pend$coefficients[[1]]*exp(m.pend$coefficients[[2]]*x),0,8, col = "#000000")+
-    # curve((m.pend$coefficients[[1]]+m.pend$coefficients[[3]])*exp((.pend$coefficients[[2]]+m.pend$coefficients[[4]])*x),0,8, col = "#E69F00")+
-    # # geom_abline(slope = m.pend$coefficients[[2]], 
-    #             intercept = m.pend$coefficients[[1]], 
-    #             alpha = 0.5,
-    #             color = "#000000") +
-    # geom_abline(slope = m.pend$coefficients[[2]]+m.pend$coefficients[[4]], 
-    #             intercept = m.pend$coefficients[[1]]+m.pend$coefficients[[3]], 
-    #             alpha = 0.5,
-    #             color = "#E69F00") +
+    # # geom_function(fun = earlevel, colour = "#000000")+
+    # geom_function(fun = bfloorlevel, colour = "#E69F00")+
+    # stat_function(fun = bearlevel, colour = "#000000")+
+    geom_line(filter(curve, condition == "Ear level"), mapping = aes(x = x2, y = y1))+
     geom_text(x = 0.2, y = 7.6, label = sub, hjust = 0, nudge_x =  0, parse = TRUE, size = 4, color = "#999999")+
     geom_text(x = 0.2, y = 7.1, label = as.character(as.expression(eq1)), hjust = 0, nudge_x =  0, parse = TRUE, size = 4, color = "#000000")+
     geom_text(x = 0.2, y = 6.7, label = as.character(as.expression(eq2)), hjust = 0, nudge_x =  0,parse = TRUE, size = 4, color = "#E69F00")+
@@ -67,8 +72,12 @@ for (i in 1:length(levels(results_tbl$subject))) {
     theme_pubr(base_size = 12, margin = TRUE)+
     theme(legend.position = "top",
           legend.title = element_blank())
-  fig_normal[[i]] = fig1
+  fig1
   
+  fig_normal[[i]] = fig1
+  graphics.off()
+  earlevel = 0
+  rm("earlevel", "floorlevel")
 }
 
 Figure1 = ggarrange(fig_normal[[1]],fig_normal[[2]],fig_normal[[3]],fig_normal[[4]],fig_normal[[5]],fig_normal[[6]],fig_normal[[7]],fig_normal[[8]],fig_normal[[9]],
